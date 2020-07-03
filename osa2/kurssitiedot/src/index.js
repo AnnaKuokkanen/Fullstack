bd85463dd3ebd1fa8eef7f6ebfdd82b66044e712
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-03 10:38:59 +0300

    teht 2.4

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -2,86 +2,106 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      <ul>
+        {courses.map(course => 
+          <Course key={course.id} name={course.name} parts={course.parts} />
+        )}
+      </ul>
     </div>
   )
 }
 
-const Course = ({ course }) => {
+const Course = ({ name, parts }) => {
   const array = []
-  for (let i=0; i<course.parts.length; i++) {
-    array.push(course.parts[i].exercises)
+  for (let j=0; j<parts.length; j++) {
+    array.push(parts[j].exercises)
   }
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+  console.log('välitetään nimi', name)
+  console.log('välitetään osia', parts)
+
   return (
     <div>
-      <h1>{ course.name }</h1>
-      <ul>
-        {course.parts.map(part =>
-          <ul key={part.name}>
-            {part.name} {part.exercises} 
-          </ul>
-        )}
-      </ul>
-        <p>total of {array.reduce(reducer)} exercises</p>
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total total={array.reduce(reducer)}/>
     </div>
   )
 }
 
-// const Header = (props) => {
-//   return (
-//     <div>
-//       <h1>{props.course.name}</h1>
-//     </div>
-//   )
-// }
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+    </div>
+  )
+}
 
-// const Part = (props) => {
-//     return (
-//         <div>
-//           <p>
-//             {props.part} {props.exercises}
-//           </p>
-//         </div>
-//     )
-// }
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <ul key={part.id}>
+          {part.name} {part.exercises}
+        </ul>
+      )}
+    </div>
+  )
+}
 
-// const Content = (props) => {
-//     return (
-//         <div>
-//           <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-//           <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-//           <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-//         </div>
-//     )
-// }
-
-// const Total = (props) => {
-//     return (
-//         <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-//     )
-// }
+const Total = ({ total }) => {
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
