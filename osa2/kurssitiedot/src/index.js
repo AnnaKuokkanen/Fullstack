e39ba352636fd94b9fd1afefabd06b0da330467e
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-03 10:57:13 +0300

    teht 2.5

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -56,51 +57,6 @@ const App = () => {
         )}
       </ul>
     </div>
-  )
-}
-
-const Course = ({ name, parts }) => {
-  const array = []
-  for (let j=0; j<parts.length; j++) {
-    array.push(parts[j].exercises)
-  }
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-  console.log('välitetään nimi', name)
-  console.log('välitetään osia', parts)
-
-  return (
-    <div>
-      <Header name={name} />
-      <Content parts={parts} />
-      <Total total={array.reduce(reducer)}/>
-    </div>
-  )
-}
-
-const Header = ({ name }) => {
-  return (
-    <div>
-      <h3>{name}</h3>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <ul key={part.id}>
-          {part.name} {part.exercises}
-        </ul>
-      )}
-    </div>
-  )
-}
-
-const Total = ({ total }) => {
-  return (
-    <p>Number of exercises {total}</p>
   )
 }
 
