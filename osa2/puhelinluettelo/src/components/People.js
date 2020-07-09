8028fdbb56d5abc85441eb3d89dac798ebea6af1
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-09 12:10:00 +0300

    teht 2.17

--- osa2/puhelinluettelo/src/components/People.js
+++ osa2/puhelinluettelo/src/components/People.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const People = ({people}) => {
+const People = ({people, onClick}) => {
     return (
       <div>
         <ul>
           {people.map(person =>
             <ul key={person.name}>
               {person.name} {person.number}
+              <button onClick={() => onClick(person.id)}>delete</button>
             </ul>
           )}
         </ul>
--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -9,12 +9,12 @@ const create = newObject => {
   return axios.post(baseUrl, newObject)
 }
 
-const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default { 
   getAll: getAll, 
   create: create, 
-  update: update 
+  remove: remove
 }
