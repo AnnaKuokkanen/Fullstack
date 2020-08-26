db4494805b9769ccc9d4befdafec0dbe6e1611ce
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-26 07:23:32 +0300

    teht 4.2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,12 +3,11 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-//const blogsRouter = require('./controllers/blogs')
-
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-//app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter)
 app.use(cors())
 app.use(express.json())
 
