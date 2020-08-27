6750c4fffbe4dfe6d1b3f46ae35b0eed1ecc062f
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-27 03:32:58 +0300

    teht 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  let likes = 0
+  blogs.forEach(blog => likes = likes + blog.likes)
+
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy, 
+  totalLikes
 }
