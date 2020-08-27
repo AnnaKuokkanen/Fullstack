6863fd674d137a24f175d33065e438cc51915fc5
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-08-27 04:10:40 +0300

    teht 4.5

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -91,3 +91,79 @@ describe('total likes', () => {
     expect(result).toBe(36)
   })
 })
+
+describe('favorite blog', () => {
+  const allBlogs = [ 
+    { 
+      _id: "5a422a851b54a676234d17f7", 
+      title: "React patterns", author: "Michael Chan", 
+      url: "https://reactpatterns.com/", 
+      likes: 7,
+      __v: 0 
+    },
+
+    { 
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful", 
+      author: "Edsger W. Dijkstra", 
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", 
+      likes: 5, 
+      __v: 0 
+    }, 
+    
+    { 
+      _id: "5a422b3a1b54a676234d17f9", 
+      title: "Canonical string reduction", 
+      author: "Edsger W. Dijkstra", 
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", 
+      likes: 12, 
+      __v: 0 
+    }
+  ]
+
+  const listWithOneBlog = [
+    { 
+      _id: "5a422bc61b54a676234d17fc", 
+      title: "Type wars", 
+      author: "Robert C. Martin", 
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", 
+      likes: 2, 
+      __v: 0 
+    }
+  ]
+
+  const emptyList = []
+
+  test('when list is empty result is null', () => {
+    const result = listHelper.favoriteBlog(emptyList)
+    expect(result).toBe(null)
+  })
+  
+  test('when list has one blog it is the result', () => {
+    const blog = { 
+      _id: "5a422bc61b54a676234d17fc", 
+      title: "Type wars", 
+      author: "Robert C. Martin", 
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", 
+      likes: 2, 
+      __v: 0 
+    }
+
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(blog)
+  })
+
+  test('when list has multiple blogs result is the most popular blog', () => {
+    const blog = { 
+      _id: "5a422b3a1b54a676234d17f9", 
+      title: "Canonical string reduction", 
+      author: "Edsger W. Dijkstra", 
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", 
+      likes: 12, 
+      __v: 0 
+    }
+
+    const result = listHelper.favoriteBlog(allBlogs)
+    expect(result).toEqual(blog)
+  })
+}) 
