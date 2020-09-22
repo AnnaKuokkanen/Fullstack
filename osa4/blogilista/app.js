f98158c7402bdbaaafc26271b42d0881f2fa2221
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-09-22 22:24:36 +0300

    teht 4.20

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -15,6 +16,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.tokenExtractor)
+app.use(middleware.errorHandler)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
---
+++ osa4/blogilista/utils/middleware.js
@@ -0,0 +1,36 @@
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message 
+    })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  }
+
+  logger.error(error.message)
+
+  next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } 
+  next()
+}
+
+module.exports = {
+    tokenExtractor,
+    errorHandler
+}
