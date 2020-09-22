82f90aef574f16c1cd5620855eb80a9819507e26
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-09-22 22:24:36 +0300

    teht 4.20

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -3,18 +3,8 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
-
   const blogs = await Blog.find({}).populate('user', {name: 1, username: 1, id: 1})
-
   response.json(blogs)
 })
   
@@ -23,9 +13,8 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).json(request.body)
   } else {
     const body = request.body
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
