8f22030b373fc455335690b92c0fa87f92bb6c34
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-09-24 02:41:44 +0300

    teht 4.22

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -5,24 +5,31 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
-    {
-      title: 'Books',
-      author: 'Mary',
-      url: 'books.com',
-      likes: 666
-    },
-    {
-      title: 'Films',
-      author: 'Charlie',
-      url: 'films.com',
-      likes: 333
-    },
-  ]
-  
+  {
+    title: 'Books',
+    author: 'Mary',
+    url: 'books.com',
+    likes: 666
+  },
+  {
+    title: 'Films',
+    author: 'Charlie',
+    url: 'films.com',
+    likes: 333
+  },
+]
+
+const initialUser = {
+  username: 'username',
+  password: 'password'
+}
+
 beforeEach(async () => {
   await Blog.deleteMany({})
+  await User.deleteMany({})
 
   let blogObject = new Blog(initialBlogs[0])
   await blogObject.save()
@@ -31,81 +38,126 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('app is returning correct amount of blogs', async () => {
-  const blogs = await api.get('/api/blogs')
-  expect(blogs.body.length === initialBlogs.length)
-})
-
-test('app is returning json formatted blogs', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('id is in the field called id', async () => {
-  const blogs = await api.get('/api/blogs')
-
-  blogs.body.forEach(blog => expect(blog.id).toBeDefined())
-})
-
-test('blog can be added to /api/blogs', async () => {
-  const newBlog = new Blog({
-    title: 'Plant life', 
-    author: 'Michael',  
-    url:'plants.com', 
-    likes: 50
+describe('Getting blogs', () => {
+  test('app is returning correct amount of blogs', async () => {
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body.length === initialBlogs.length)
   })
-    
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)   
 
-  const allBlogs = await api.get('/api/blogs')
+  test('app is returning json formatted blogs', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('id is in the field called id', async () => {
+    const blogs = await api.get('/api/blogs')
   
-  const blogs = allBlogs.body.map(b => [b.title, b.author, b.url, b.likes])
-  const blog = [newBlog.title, newBlog.author, newBlog.url, newBlog.likes]
-
-  expect(blogs).toHaveLength(initialBlogs.length + 1)
-  expect(blogs).toContainEqual(blog)
+    blogs.body.forEach(blog => expect(blog.id).toBeDefined())
+  })
 })
 
-test('when likes are not specified, they are zero', async () => {
-  const newBlog = {
-    title: 'Plant life', 
-    author: 'Michael',  
-    url:'plants.com'
-  }
+describe('Posting blogs', () => {
+  test('blog can be added to /api/blogs', async () => {
+    const newBlog = new Blog({
+      title: 'Plant life', 
+      author: 'Michael',  
+      url:'plants.com', 
+      likes: 50
+    })
+  
+    await api
+          .post('/api/users')
+          .send(initialUser)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/) 
+    const partToken = await api
+                    .post('/api/login')
+                    .send({ username: 'username', password: 'password' })
 
-  const allBlogs = await api.get('/api/blogs')
+    const token = 'bearer ' + partToken.body.token
 
-  const blogs = allBlogs.body.map(b => [b.title, b.author, b.url, b.likes])
-  const blog = [newBlog.title, newBlog.author, newBlog.url, 0]
+    console.log('TOKEN ', token)
 
-  console.log('BLOGIT', blogs)
+    await api
+      .post('/api/blogs')
+      .set('Authorization', token)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)   
+  
+    const allBlogs = await api.get('/api/blogs')
+    
+    const blogs = allBlogs.body.map(b => [b.title, b.author, b.url, b.likes])
+    const blog = [newBlog.title, newBlog.author, newBlog.url, newBlog.likes]
+  
+    expect(blogs).toHaveLength(initialBlogs.length + 1)
+    expect(blogs).toContainEqual(blog)
+  })
 
-  expect(blogs).toHaveLength(initialBlogs.length + 1)
-  expect(blogs).toContainEqual(blog)
-})
+  test('if blog does not have title and url it is rejected', async () => {
+    const newBlog = {
+      author: 'Michael',  
+      likes: 2
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })  
 
-test('if blog does not have title and url it is rejected', async () => {
-  const newBlog = {
-    author: 'Michael',  
-    likes: 2
-  }
+  test('when likes are not specified, they are zero', async () => {
+    const newBlog = {
+      title: 'Plant life', 
+      author: 'Michael',  
+      url:'plants.com'
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    await api
+    .post('/api/users')
+    .send(initialUser)
+
+    const partToken = await api
+                      .post('/api/login')
+                      .send({ username: 'username', password: 'password' })
+
+    const token = 'bearer ' + partToken.body.token
+  
+    await api
+      .post('/api/blogs')
+      .set('Authorization', token)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/) 
+  
+    const allBlogs = await api.get('/api/blogs')
+  
+    const blogs = allBlogs.body.map(b => [b.title, b.author, b.url, b.likes])
+    const blog = [newBlog.title, newBlog.author, newBlog.url, 0]
+  
+    expect(blogs).toHaveLength(initialBlogs.length + 1)
+    expect(blogs).toContainEqual(blog)
+  })
+
+  test('when token is missing or invalid blog cannot be added', async () => {
+    const newBlog = {
+      title: 'Plant life', 
+      author: 'Michael',  
+      url:'plants.com'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', 'abc')
+      .send(newBlog)
+      .expect(401)
+  })
 })
 
 afterAll(() => {
