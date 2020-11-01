a27ddc1cf1ce3a72268114a00429a99876eb1076
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-11-01 18:17:09 +0200

    teht 5.1

---
+++ osa5/client/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [logged, setLogged] = useState(false)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    setLogged(true)
+  }
+
+  return (
+    <div>
+      {logged ? (
+        <div>
+          <h2>blogs</h2>
+          <p>{username} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      ) : (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              username
+                <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              password
+                <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
