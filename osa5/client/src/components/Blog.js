db7d67ff61c711b28883f93aff7402de823236a1
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-11-17 23:58:07 +0200

    teht 5.7

--- osa5/client/src/components/Blog.js
+++ osa5/client/src/components/Blog.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+
   Blog.propTypes = {
     blog: PropTypes.object.isRequired
   }
 
+  const handleShow = () => {
+    setShow(!show)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   return (
-    <div>
-    {blog.title} {blog.author}
+    <div style={blogStyle}>
+      <p>{blog.title} by {blog.author}</p>
+      {show ? (
+        <div>
+          {blog.url}
+          <p>likes {blog.likes}</p><button>like</button>
+          <button onClick={handleShow}>hide</button>
+        </div>
+      ) : (
+        <button onClick={handleShow}>view</button>
+      )} 
   </div>
   )
 }
